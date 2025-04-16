6318897d910b97bb30bdd812c8f397657244de56
Tina Landar <landartina@gmail.com>
2025-04-16 14:33:22 +0300

    fourth-commit

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,30 +7,38 @@ let gallery = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 function createGallery(images) {
-  for (const image of images) {
-    ul.insertAdjacentHTML(
-      'beforeend',
-      `<li><a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" title=""/>
-    <table>
-  <thead>
-    <tr class="th">
-      <th>Likes</th>
-      <th>Views</th>
-      <th>Comments</th>
-      <th>Downloads</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>${image.likes}</td>
-      <td>${image.views}</td>
-      <td>${image.comments}</td>
-      <td>${image.downloads}</td>
-    </tr></tbody>
-    </a></li>`
-    );
-    gallery.refresh();
-  }
+  let markup = '';
+
+  images.forEach(image => {
+    markup += `
+    <li>
+      <a href="${image.largeImageURL}">
+        <img src="${image.webformatURL}" alt="${image.tags}" title="" />
+        <table>
+          <thead>
+            <tr class="th">
+              <th>Likes</th>
+              <th>Views</th>
+              <th>Comments</th>
+              <th>Downloads</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${image.likes}</td>
+              <td>${image.views}</td>
+              <td>${image.comments}</td>
+              <td>${image.downloads}</td>
+            </tr>
+          </tbody>
+        </table>
+      </a>
+    </li>
+  `;
+  });
+
+  ul.insertAdjacentHTML('beforeend', markup);
+  gallery.refresh();
 }
 export default createGallery;
 
--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import { showLoader, hideLoader, clearGallery } from './js/render-functions';
 
-const ul = document.querySelector('.gallery');
 const form = document.querySelector('.form');
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   clearGallery();
@@ -16,14 +16,18 @@ form.addEventListener('submit', e => {
     showLoader();
     getImagesByQuery(userWrited.value)
       .then(images => {
-        createGallery(images);
+        if (images.length != 0) {
+          createGallery(images);
+        } else {
+          iziToast.error({
+            title: '',
+            message: `Sorry, there are no images matching your search query. Please try again!`,
+            position: 'topRight',
+          });
+        }
       })
-      .catch(() => {
-        iziToast.error({
-          title: '',
-          message: `Sorry, there are no images matching your search query. Please try again!`,
-          position: 'topRight',
-        });
+      .catch(error => {
+        return error;
       })
       .finally(() => {
         hideLoader();
